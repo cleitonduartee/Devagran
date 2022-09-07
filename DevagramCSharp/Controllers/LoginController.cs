11a9324df9b8fafd1a678db0f1ca976704315a9b
cdgoncalves <cdgoncalves@MILTEC-0381>
2022-09-07 16:18:41 -0400

    Criacao do Token Service

--- DevagramCSharp/Controllers/LoginController.cs
+++ DevagramCSharp/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using DevagramCSharp.Controllers.Dtos;
+using DevagramCSharp.Models;
+using DevagramCSharp.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,7 +23,40 @@ namespace DevagramCSharp.Controllers
         {
             try
             {
-                throw new ArgumentException("Erro ao preencher os dados");
+                if(!String.IsNullOrEmpty(login.Email) && !String.IsNullOrEmpty(login.Senha))
+                {
+                    string email = "cleiton@devaria.com.br";
+                    string senha = "Senha@123";
+
+                    if(email.Equals(login.Email) && senha.Equals(login.Senha))
+                    {
+                        Usuario usuario = new Usuario()
+                        {
+                            Email = login.Email,
+                            Id = 12,
+                            Nome = "Cleiton Durte"
+                        };
+                        return Ok(new LoginRespostaDto()
+                        {
+                            Email = usuario.Email,
+                            Nome = usuario.Nome,
+                            Token = TokenService.CriarToken(usuario)
+                        });
+                    }
+                    return BadRequest(new ErrorRespostaDto()
+                    {
+                        Descricao = "Email ou Senha inválido",
+                        Status = StatusCodes.Status400BadRequest
+                    });
+                }
+                else
+                {
+                    return BadRequest(new ErrorRespostaDto()
+                    {
+                        Descricao = "Usuário não preencheu os campo de login corretamente.",
+                        Status = StatusCodes.Status400BadRequest
+                    });
+                }
                 
             }catch(Exception ex)
             {
